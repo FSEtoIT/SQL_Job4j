46d201f0dcd0f4e274ad02e4f7eea50d3883f90e
FSE <amakeevmindraycom@yandex.ru>
2026-06-17 17:38:45 +0300

    1. Связи между таблицами [#373615]

--- db/SQL_db.sql
+++ db/SQL_db.sql
@@ -1,21 +1,37 @@
-CREATE TABLE first_table(
-    id serial PRIMARY KEY,
-    name VARCHAR(255),
-    title TEXT,
-    age INTEGER
+CREATE TABLE clients(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO first_table (name, title, age) 
-VALUES ('Andrey', 'Student', '37');
+CREATE TABLE issues(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
 
-SELECT * FROM first_table;
+CREATE TABLE clients_issues(
+    id SERIAL PRIMARY KEY,
+    client_id INT REFERENCES clients(id) ON DELETE CASCADE,
+    issue_id INT REFERENCES issues(id) ON DELETE CASCADE
+);
 
-UPDATE first_table SET name = 'Petr';
-UPDATE first_table SET title = 'Mentor';
-UPDATE first_table SET age = '36';
+INSERT INTO clients(name) VALUES
+('Ivan'),
+('Kirill'),
+('Roman');
 
-SELECT * FROM first_table;
 
-DELETE from first_table;
+INSERT INTO issues(title, description) VALUES
+('Can not log in to server', 'He may be tried to swith on & off his computer or wi-fi or even head-ass primary swith. Need ask more details'),
+('Used AI and fucked up', 'Looks like does not understand the foundation of his class. Ask him read some books and re-do previous course'),
+('CheckStyle errors', 'Invalid CREATE statement 1 / Invalid CREATE TABLE statement 1 / Invalid Table columns clause 1 / expected one of the following: ... 3 / Invalid or incomplete statement 3');
 
-SELECT * FROM first_table;
+INSERT INTO clients_issues(client_id, issue_id) VALUES
+(1, 1),
+(1, 2),
+(1, 3),
+(2, 1),
+(2, 2),
+(3, 3);
+
+SELECT * FROM clients_issues;
